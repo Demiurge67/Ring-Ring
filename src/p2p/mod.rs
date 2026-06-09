3d8f85f505c3b47ca9047de0d79cf5bedf8340c3
Dem Demiurge <51953496+Demiurge67@users.noreply.github.com>
2026-06-09 15:13:28 +0400

    Update mod.rs

--- src/p2p/mod.rs
+++ src/p2p/mod.rs
@@ -3,5 +3,9 @@
 pub mod node;
 pub mod dht;        // заглушка
 pub mod behaviour;  // заглушка
+pub mod transport;
+pub mod discovery;
 
+pub use transport::RingTransport;
+pub use discovery::{publish_service, discover_peers};
 pub use node::{RingNode, NodeConfig};
